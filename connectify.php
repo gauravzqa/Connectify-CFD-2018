4df85557ab4e7bed400cd70942f323d77708f0a0
gauravzqa <gauravzqa@gmail.com>
2018-02-05 03:34:03 +0530

    FIRST PUSH

---
+++ connectify.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connectify</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+</head>
+<body>
+<div class="Con-Webbies"   >
+	<h1 id="Con-Webbies" >Team Con-Webbies </h2>
+</div>
+	<div class="wrapper" >
+		<div class="team" >
+			<div class="a" >
+				<div class ="member" >
+					<img src="images/aniket.jpg" width="200px" height="200px" style="border-radius:50%;">
+				</div>
+				<div class="about" >
+					<p>Aniket Mathur</p>
+				</div>
+				<div class="follow" style="display: inline-flex; " >
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/facebook.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/git.svg" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/gmail.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/linkedin.png" width=30px" height=30px"></a></div>
+				</div>
+			</div>
+			<div class ="a" >
+				<div class="member" >
+					<img src="images/gaurav.jpeg" width="200px" height="200px" style="border-radius:50%;">
+				</div>
+				<div class="about" >
+					<p>gaurav pal</p>
+				</div>
+				<div class="follow" style="display: inline-flex; " >
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/facebook.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/git.svg" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/gmail.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/linkedin.png" width=30px" height=30px"></a></div>
+				</div>
+			</div>
+			<div class ="a" >
+				<div class="member" >
+					<img src="images/manky.jpg" width="200px" height="200px" style="border-radius:50%;">
+				</div>
+				<div class="about" >
+					<p>mayank mehta</p>
+				</div>
+				<div class="follow" style="display: inline-flex; " >
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/facebook.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/git.svg" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/gmail.png" width=30px" height=30px"></a></div>
+					<div class="fb" style="margin: 15px;" ><a href=""><img src="images/linkedin.png" width=30px" height=30px"></a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>

3c585275bb9e48c5f3a8aed7ef4cd7b4ac8f6f3b
aniket21mathur <aniketmathur320@gmail.com>
2018-02-06 06:43:45 +0530

    new add

--- 1.php
+++ 1.php
@@ -24,9 +24,39 @@
 </div>
 <div class="row d">
 	<div class="col-sm-12 e">
+		our aim is to connect
+	</div>
+</div>
+<div clas="row ">
+	<div class="col-sm-12 aa">
+	<span style="border-bottom: 2px solid #efefef;padding-bottom: 3px;">ABOUT CONNECTIFY</span>
+</div>
+</div>
+<br>
+<div class="row" style="background-color: white">
+	<div class="col-sm-2"></div>
+	<div class="col-sm-8 ab">
+		The aim of the project is to make a connection among webbies in a quest for learning as well as sharing knowledge and skills. It is a platform where one can know the involvement of the second or the third person in different work areas, his/her skills, achievements and can get in touch with him/her. "Learn as you share, share as you grow!"
+		<br><br>
+	</div>
+	<div class="col-sm-2"></div>
+</div>
+<div class="row ac">
+	<div class="col-sm-4 ac1">
+		
+	</div>
+	<div class="col-sm-4 ac2">
+		
+	</div>
+	<div class="col-sm-4 ac3">
+		
+	</div>
+</div>
+<div class="row ad">
+	<div class="col-sm-12  ad1">
+		
 	</div>
 </div>
 </div>
-</div>	
 </body>
 </html>
